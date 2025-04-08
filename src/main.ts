ee9f5a533ee71d3805a483f82ee1b98a3732ac35
kutaiba <kalnizaemy@darsoft.net>
2025-04-08 13:41:41 +0300

    change on user folder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000).then(()=>{
+    const logger = new Logger("APP")
+    logger.log(`\n\nthe nestjs server work on : http://localhost:${process.env.PORT}`)
+  });
 }
 
 void bootstrap();
